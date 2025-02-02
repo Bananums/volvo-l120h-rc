bd8414097efcda03c324772400e0cd1b66297056
JSRobotics <sjohnsen.valle@gmail.com>
2025-02-02 19:44:36 +0100

    Added additional assert

--- host/src/main.cc
+++ host/src/main.cc
@@ -4,9 +4,34 @@
 #include <termios.h>    // For termios, tcgetattr(), tcsetattr(), cfsetispeed(), cfsetospeed(), cfmakeraw()
 #include <cstring>
 #include <cerrno>
+#include <cassert>
+
+bool IsValidBaudRate(const speed_t baud_rate) {
+    switch (baud_rate) {
+        case B57600:
+        case B115200:
+        case B230400:
+        case B460800:
+        case B500000:
+        case B576000:
+        case B921600:
+        case B1000000:
+        case B1152000:
+        case B1500000:
+        case B2000000:
+        case B2500000:
+        case B3000000:
+        case B3500000:
+        case B4000000:
+            return true;
+        default:
+            return false;
+    }
+}
 
 
 bool OpenSerialPort(const char *port_name, int &file_descriptor) {
+    assert(port_name != nullptr && "port_name must not be null!");
     file_descriptor = open(port_name, O_RDWR);
 
     if (file_descriptor < 0) {
@@ -16,6 +41,9 @@ bool OpenSerialPort(const char *port_name, int &file_descriptor) {
 }
 
 bool ConfigureSerialPort(const int file_descriptor, const speed_t baud_rate) {
+    assert(file_descriptor >= 0 && "file_descriptor must be valid (file_descriptor >= 0)!");
+    assert(IsValidBaudRate(baud_rate) && "Invalid baud_rate passed to ConfigureSerialPort!");
+
     termios tty{};
     if (tcgetattr(file_descriptor, &tty) != 0) {
         return false;
@@ -61,6 +89,9 @@ void WaitForAllSerialTransmitted(const int file_descriptor) {
 }
 
 ssize_t WriteSerialData(const int file_descriptor, const char *buffer) {
+    assert(buffer != nullptr && "buffer must not be null!");
+    assert(file_descriptor >= 0 && "file_descriptor must be valid (>= 0)!");
+
     const ssize_t bytes_written = write(file_descriptor, buffer, strlen(buffer));
     return bytes_written;
 }
@@ -75,13 +106,12 @@ int main() {
         return 1;
     }
 
-    if (!ConfigureSerialPort(serial_port, 115200)) {
+    if (!ConfigureSerialPort(serial_port, B115200)) {
         std::cerr << "Failed to configure serial port " << uart_port_name << ": " << strerror(errno) << std::endl;
         CloseSerialPort(serial_port);
     }
 
-
-    const char *data = "Helllo World!";
+    const char *data = "Hello World!";
     const ssize_t bytes_written = WriteSerialData(serial_port, data);
     if (bytes_written < 0) {
         std::cerr << "Failed to write to serial port " << uart_port_name << ": " << strerror(errno) << std::endl;
